ddf5cc4b90f387a16365ee70a5a32525fcff0037
Laura Rodrigues <136133071+laurarodriguesd@users.noreply.github.com>
2024-01-26 16:35:22 -0300

    Add files via upload

---
+++ Projeto Spotify - Alura React/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './Header/Header';
+import Sidebar from './Sidebar/Sidebar';
+import Footer from './Footer/Footer';
+import Main from './Main/Main';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Sidebar/>
+      <Main/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
+
